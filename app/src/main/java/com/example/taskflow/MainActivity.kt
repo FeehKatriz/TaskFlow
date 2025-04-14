29a86666dc34d86ed20822fe5af42af06cee07a8
Feeh Katriz <fernandokristos@gmail.com>
2025-04-14 20:50:50 -0300

    Atualizando projeto e deixando Pronto
    Nessa Etapa, Colocamos o ViewBinding, para facilidar a inserção e também estamoss deixando as tela com um quebra coreta

--- app/src/main/java/com/example/taskflow/MainActivity.kt
+++ app/src/main/java/com/example/taskflow/MainActivity.kt
@@ -7,28 +7,31 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.taskflow.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
 
-    private lateinit var btnClique: Button
+    private val binding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
 
-
-
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        setContentView(binding.root)
 
-        btnClique = findViewById(R.id.btn_click)
+        with(binding) {
+           btnClick.setOnClickListener {  }
+           btnExecutar?.setOnClickListener {  }
+       }
 
-        btnClique.setOnClickListener{
-        }
+
     }
 }
