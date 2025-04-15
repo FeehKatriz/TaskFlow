7e8cfb719e7e53eb05e9ae5730affe148d1cb683
Feeh Katriz <fernandokristos@gmail.com>
2025-04-14 21:48:23 -0300

    atualizando btns para navegação
    Aqui deixei o método de controle basico para conseguir passar as activits para um lado ou outro (navegação basica)

--- app/src/main/java/com/example/taskflow/MainActivity.kt
+++ app/src/main/java/com/example/taskflow/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.taskflow
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -22,10 +23,13 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        val intent = Intent(this, teste2::class.java)
         setContentView(binding.root)
 
         with(binding) {
-
+            btnAbrir?.setOnClickListener {
+                startActivity(intent)
+            }
         }
     }
 }
--- app/src/main/java/com/example/taskflow/teste2.kt
+++ app/src/main/java/com/example/taskflow/teste2.kt
@@ -1,20 +1,32 @@
 package com.example.taskflow
 
 import android.os.Bundle
+import android.renderscript.ScriptGroup
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.viewbinding.ViewBinding
+import com.example.taskflow.databinding.ActivityMainBinding
 
 class teste2 : AppCompatActivity() {
+   private val binding by lazy {
+       ActivityMainBinding.inflate(layoutInflater)
+   }
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_teste2)
+        setContentView(binding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        with(binding){
+    
         }
     }
 }
