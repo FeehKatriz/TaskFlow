310826acfe957ce61c573ccd8bf263f23bc75df7
Feeh Katriz <fernandokristos@gmail.com>
2025-04-14 21:24:48 -0300

    a
    a

--- app/src/main/java/com/example/taskflow/MainActivity.kt
+++ app/src/main/java/com/example/taskflow/MainActivity.kt
@@ -1,8 +1,6 @@
 package com.example.taskflow
 
 import android.os.Bundle
-import android.widget.Button
-import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,16 +9,15 @@ import com.example.taskflow.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-
     private val binding by lazy {
         ActivityMainBinding.inflate(layoutInflater)
-
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+
+        ViewCompat.setOnApplyWindowInsetsListener(binding.root) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
@@ -28,10 +25,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         with(binding) {
-           btnClick.setOnClickListener {  }
-           btnExecutar?.setOnClickListener {  }
-       }
 
-
+        }
     }
 }
