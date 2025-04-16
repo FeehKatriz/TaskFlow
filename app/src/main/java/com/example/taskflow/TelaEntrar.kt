24276cd58452681bcf42e935d54b3f1952d34335
Feeh Katriz <fernandokristos@gmail.com>
2025-04-15 22:22:18 -0300

    depressao

--- app/src/main/java/com/example/taskflow/TelaEntrar.kt
+++ app/src/main/java/com/example/taskflow/TelaEntrar.kt
@@ -1,35 +1,20 @@
 package com.example.taskflow
 
 import android.os.Bundle
-import android.renderscript.ScriptGroup
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import androidx.viewbinding.ViewBinding
-import com.example.taskflow.databinding.ActivityTeste2Binding
 
-class teste2 : AppCompatActivity() {
-   private val binding by lazy {
-       ActivityTeste2Binding.inflate(layoutInflater)
-   }
-
-
+class TelaEntrar : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(binding.root)
-        ViewCompat.setOnApplyWindowInsetsListener(binding.root) { v, insets ->
+        setContentView(R.layout.activity_tela_entrar)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
-        }
-
-       with(binding){
-           btnFechar?.setOnClickListener {
-               finish()
-           }
-
         }
     }
 }
--- app/src/main/java/com/example/taskflow/TelaEntrarPopUp.kt
+++ app/src/main/java/com/example/taskflow/TelaEntrarPopUp.kt
@@ -1,36 +1,20 @@
 package com.example.taskflow
 
-import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import com.example.taskflow.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
-
-    private val binding by lazy {
-        ActivityMainBinding.inflate(layoutInflater)
-    }
-
+class TelaEntrarPopUp : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-
-        ViewCompat.setOnApplyWindowInsetsListener(binding.root) { v, insets ->
+        setContentView(R.layout.activity_tela_entrar_pop_up)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-
-        setContentView(binding.root)
-
-        /*with(binding) {
-
-            btnAbrir?.setOnClickListener {
-                startActivity(intent)
-            }
-        }*/
     }
 }
