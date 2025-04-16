7e27e2453320c784d3024bb21254b59e93a9d1be
Feeh Katriz <fernandokristos@gmail.com>
2025-04-16 11:12:20 -0300

    scr
    aa

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,4 +51,8 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+    implementation("com.github.bumptech.glide:glide:4.16.0")
+    annotationProcessor("com.github.bumptech.glide:compiler:4.16.0")
+    implementation ("de.hdodenhof:circleimageview:3.1.0")
+
 }
--- app/src/main/java/com/example/taskflow/TelaPricipal.kt
+++ app/src/main/java/com/example/taskflow/TelaPricipal.kt
@@ -1,10 +1,13 @@
 package com.example.taskflow
 
 import android.os.Bundle
+import android.widget.ImageView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
 
 class TelaPricipal : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,6 +18,15 @@ class TelaPricipal : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+
         }
+        val imageView = findViewById<ImageView>(R.id.imageView16)
+
+        Glide.with(this)
+            .load(R.drawable.usertype) // ou uma URL se preferir
+            .apply(RequestOptions.circleCropTransform()) // <-- imagem redonda
+            .into(imageView)
+
     }
+
 }
