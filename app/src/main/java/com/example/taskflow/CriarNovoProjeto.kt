c8860dc07431babcbcf0a1d7966705a4f56d1e93
Feeh Katriz <fernandokristos@gmail.com>
2025-04-18 20:54:39 -0300

    scr
    scr teste

--- app/src/main/java/com/example/taskflow/CriarNovoProjeto.kt
+++ app/src/main/java/com/example/taskflow/CriarNovoProjeto.kt
@@ -10,7 +10,7 @@ class CriarNovoProjeto : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_criar_novo_projeto)
+        setContentView(R.layout.activity_tela_criar_novo_projeto)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
