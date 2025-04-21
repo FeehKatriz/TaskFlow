3264b9b1cf6828eb20fc63c535597128628a85e9
Feeh Katriz <fernandokristos@gmail.com>
2025-04-21 15:46:09 -0300

    Criando e utilizando Fragments para tela principal
    A navbar está feita, porém há muito o que melhorar!

--- app/src/main/java/com/example/taskflow/TelaPricipal.kt
+++ app/src/main/java/com/example/taskflow/TelaPricipal.kt
@@ -1,32 +1,44 @@
 package com.example.taskflow
 
 import android.os.Bundle
-import android.widget.ImageView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
-import com.bumptech.glide.Glide
-import com.bumptech.glide.request.RequestOptions
+import androidx.fragment.app.Fragment
+import com.example.taskflow.databinding.ActivityTelaPricipalBinding
+import com.example.taskflow.fragments.EquipeFragment
+import com.example.taskflow.fragments.TelaInicialFragment
 
-class TelaPricipal : AppCompatActivity() {
+class TelaPrincipal : AppCompatActivity() {
+
+    private lateinit var binding: ActivityTelaPricipalBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_tela_pricipal)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.teste)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
 
+        binding = ActivityTelaPricipalBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        replaceFragment(TelaInicialFragment()) // mostra um fragmento inicial
+
+        binding.bottomNavegation.setOnItemSelectedListener { menuItem ->
+            when (menuItem.itemId) {
+                R.id.bottom_equipe -> {
+                    replaceFragment(EquipeFragment())
+                    true
+                }
+                R.id.bottom_perfil -> {
+                    replaceFragment(TelaInicialFragment())
+                    true
+                }
+                else -> false
+            }
         }
-        val imageView = findViewById<ImageView>(R.id.imageView16)
-
-        Glide.with(this)
-            .load(R.drawable.usertype) // ou uma URL se preferir
-            .apply(RequestOptions.circleCropTransform()) // <-- imagem redonda
-            .into(imageView)
-
     }
 
+    private fun replaceFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.frame_container, fragment)
+            .commit()
+    }
 }
