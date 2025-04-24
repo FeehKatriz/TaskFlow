8cba8b67af7fc7391bc562bab5410d65c8e49cde
gabriel-2470 <gabrielfernando224444@hotmail.com>
2025-04-24 12:55:25 -0300

    Item de membros e correção de alguns erros
    criei uma recycler e um item de membros na tela de membros so pra ja deixar mais dinamico msm, qualquer coisa se for criar uma fragment é só reutilizar, tambem tive que excluir a classe da tela de edição de perfil e renomear os IDs dos reusables por que estavam dando conflito na hora de rodar o app

--- app/src/main/java/com/example/taskflow/telaEquipeMembros.kt
+++ app/src/main/java/com/example/taskflow/telaEquipeMembros.kt
@@ -5,16 +5,28 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.taskflow.databinding.ActivityTelaEquipeMembrosBinding
+import com.example.taskflow.adapter.MembroAdapter
+
 
 class telaEquipeMembros : AppCompatActivity() {
+    private val binding by lazy {
+        ActivityTelaEquipeMembrosBinding.inflate(layoutInflater)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_tela_equipe_membros)
+        setContentView(binding.root)
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.teste)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        binding.rvMembros.layoutManager = LinearLayoutManager(this)
+        binding.rvMembros.adapter = MembroAdapter()
     }
 }
