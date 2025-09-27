179d4bb962675a5c75f7b8298683f06fbe767026
fernandacarril <152343631+fernandacarril@users.noreply.github.com>
2025-09-27 15:56:22 -0300

    Removendo conflitos

--- app/src/main/java/com/example/taskflow/ui/projetos/ProjetoFragment.kt
+++ app/src/main/java/com/example/taskflow/ui/projetos/ProjetoFragment.kt
@@ -20,16 +20,12 @@ import com.example.taskflow.R
 import com.example.taskflow.adapters.EquipesProjetoAdapter
 import com.example.taskflow.adapters.MembroAdapter
 import com.example.taskflow.databinding.FragmentProjetoBinding
-import com.example.taskflow.fragments.ARG_PARAM1
-import com.example.taskflow.fragments.ARG_PARAM2
+//import com.example.taskflow.fragments.ARG_PARAM1
+//import com.example.taskflow.fragments.ARG_PARAM2
 import com.example.taskflow.models.Equipe
-import com.example.taskflow.ui.equipes.CriarNovaEquipeActivity
+import com.example.taskflow.ui.equipes.CriarEquipeActivity
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
-<<<<<<< Updated upstream:app/src/main/java/com/example/taskflow/ui/projetos/ProjetoFragment.kt
-=======
-import com.example.taskflow.ui.equipes.CriarEquipeActivity
->>>>>>> Stashed changes:app/src/main/java/com/example/taskflow/fragments/ProjetoFragment.kt
 import kotlin.random.Random
 
 class ProjetoFragment : Fragment() {
